1c7a9baec3b28ec238d01e75ac5e480973537d20
João Gouveia <a13734@ebsaas.com>
2019-03-29 21:38:56 +0000

    Finish Ficha20

---
+++ Ficha21/components/TheHeader/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <TheSideNavToggle @Toggle="$emit('sidenavToggle')" />
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+import TheSideNavToggle from '@/Components/Navigation/TheSideNavToggle';
+
+export default {
+    name: "TheHeader",
+    components: {
+        Logo,
+        TheSideNavToggle
+    }
+}
+</script>
