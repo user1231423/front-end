e7d60236f186eb10a43306819be3913ec7e9d451
João Gouveia <a13734@ebsaas.com>
2019-03-29 21:46:53 +0000

    Start Ficha 22

---
+++ Ficha22/pages/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <header class="main-header">
+    <nav class="main-nav">
+      <ul class="nav-links">
+        <nuxt-link to="/" tag="li" class="nav-link"><a>All Posts</a></nuxt-link>
+        <nuxt-link to="/about" tag="li" class="nav-link"><a>About</a></nuxt-link>
+      </ul>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <app-quote>
+            <h2>The Quote</h2>
+            <p>A wounderful Quotes</p>
+          </app-quote>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+import Quote from '@/components/Quote/quote'
+
+export default {
+  components: {
+    Logo,
+    appQuote: Quote
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
+<style scoped>
+  .main-header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background: #022d30;
+    height: 4.5rem;
+  }
+
+  .main-nav{
+    height: 100%;
+  }
+
+  .nav-links{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .nav-link{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 1rem;
+    padding: 0.3rem;
+  }
+
+  .nav-link a{
+    display: block;
+    text-decoration: none;
+    color: white;
+  }
+
+  .nav-link a:hover,
+  .nav-link a:active,
+  .nav-link.nuxt-link-exact-active a{
+    color: #06c4d1;
+  }
+
+  .nav-link.nuxt-link-exact-active {
+    border-bottom: 3px solid #06c4d1;
+  }
+</style>
