b22b03782b3a6331ba291d3ac0adfbbb2efdef98
João Gouveia <a13734@ebsaas.com>
2019-05-07 16:32:15 +0100

    Submit login form

--- Ficha9/Vue/src/components/Auth/SignIn.vue
+++ Ficha9/Vue/src/components/Auth/SignIn.vue
@@ -8,17 +8,21 @@
                         <v-spacer></v-spacer>
                     </v-toolbar>
                     <v-card-text>
-                        <v-form v-on:submit="login">
+                        <v-form v-on:@submit="login">
                             <v-text-field prepend-icon="person" name="email" label="Email" type="text"></v-text-field>
                             <v-text-field prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-flex text-center>
+                                    <v-btn type="submit" color="primary">Login</v-btn>
+                                    <br>
+                                    <router-link to="/signup">
+                                        Registar
+                                    </router-link>
+                                </v-flex>
+                            </v-card-actions>
                         </v-form>
                     </v-card-text>
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-flex text-center>
-                            <v-btn type="submit" color="primary">Login</v-btn>
-                        </v-flex>
-                    </v-card-actions>
                 </v-card>
             </v-flex>
         </v-layout>
