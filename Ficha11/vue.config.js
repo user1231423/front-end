58ed73258938eb893557922b1a77ba3649178f2d
João Gouveia <a13734@ebsaas.com>
2019-05-24 18:38:59 +0100

    Ficha 11 can't build

--- Ficha11/vue.config.js
+++ Ficha11/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest'
-  }
+  },
+  publicPath: undefined,
+  outputDir: '../wwwRoot',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
