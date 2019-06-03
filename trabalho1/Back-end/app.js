15e112932490b039e941b835d5ec1895e4fdd7e3
Joao Gouveia <joaogouveia.tc24@gmail.com>
2019-06-03 16:59:36 +0100

    Add back-end to trabalho1

--- trabalho1/Back-end/app.js
+++ trabalho1/Back-end/app.js
@@ -1,27 +1,43 @@
+var createError = require('http-errors');
 var express = require('express');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var path = require('path');
 var cookieParser = require('cookie-parser');
-var passport = require('passport');
-var flash = require('connect-flash');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true }));
+//View setup
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } })); // Use the session middleware
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+//Components to use
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-// routes ======================================================================
-require('./config/passport')(passport); // pass passport for configuration
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+//Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// express server
-var server = app.listen(3000, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Example app listening at http://%s:%s", host, port);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ trabalho1/Front-end/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    lintOnSave: false,
+    devServer: {
+        proxy: 'http://localhost:3000'
+    },
+    publicPath: undefined,
+    outputDir: '../Back-end/views',
+    assetsDir: undefined,
+    runtimeCompiler: undefined,
+    productionSourceMap: undefined,
+    parallel: undefined,
+    css: undefined
+}
